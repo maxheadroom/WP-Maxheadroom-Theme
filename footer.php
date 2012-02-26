90aa98885caa47c5114fee05baaf88c48ed2c28d
Falko Zurell <falko.zurell@gmail.com>
2012-02-26 11:17:29 +0100

    fixed footer and added license terms as well as link to theme homepage on github

--- footer.php
+++ footer.php
@@ -2,6 +2,9 @@
 				<div id="footer">
 					<ul id="credits">
 						<li><a href="http://wordpress.org" class="wordpress">Powered by Wordpress</a></li>
+						<li>
+						<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Dataset" property="dct:title" rel="dct:type">WP-Maxheadroom-Theme</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/maxheadroom/WP-Maxheadroom-Theme" property="cc:attributionName" rel="cc:attributionURL">Falko Zurell</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/maxheadroom/WP-Maxheadroom-Theme" rel="dct:source">github.com</a>.
+						</li>
 					</ul>
 					<p id="back-top"><a href="#wrapper">Top</a></p>
 				</div>
