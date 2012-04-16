0f60bae234cd9c0cc1f948032ed5c8ce397b4007
Falko Zurell <fzurell@webserver.explain-it.org>
2012-04-16 09:43:47 +0200

    added a gitignore file to exclude personalized header file. The header now includes a file with personalized header content (if present) this allows for template updates without loosing local changes.

--- header.php
+++ header.php
@@ -7,6 +7,14 @@
 	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="screen" />
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css' />
+
+	<!-- 
+		insert personalized header content in the separate file 
+		create the file: personalized-header.php
+		see also: http://codex.wordpress.org/Function_Reference/get_template_part
+	-->
+	<?php get_template_part( 'personalized', 'header' ); ?>
+	<!-- end personalized content -->
 	
 	<!-- insert WordPress header stuff -->
 	<?php wp_head(); ?>
